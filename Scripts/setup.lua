c2df6705c166ffbba47675c79cf45911b617f7c3
SkySom <skylar.som@gmail.com>
2014-01-16 23:22:50 -0500

    Added Current Scripts
    Just adding Lorei's current scripts in. Not removing them from the main
    project. Not quite ready.

---
+++ Scripts/setup.lua
@@ -0,0 +1,11 @@
+--luanet.load_assembly("System.Speech.Recognition")
+
+--Setup lorei's keywords--
+RegisterLoreiName("System")
+RegisterLoreiName("Lorei")
+RegisterLoreiName("Viki")
+--RegisterLoreiName("Endora")
+
+--Setup Functions--
+RegisterLoreiFunction("Launch")
+RegisterLoreiFunction("Close")
